06f168e92446a8122e811fc87320924fd4ea36c2
michael21910 <faefae31910@gmail.com>
2021-07-28 15:45:53 +0800

    UVa 10268 - 498-bis solved

--- UVa 10268 - 498-bis/498-bis.cpp
+++ UVa 10268 - 498-bis/498-bis.cpp
@@ -2,34 +2,27 @@
 
 using namespace std;
 
+int a[1000000];
+
+void run(int length, int x)
+{
+	long long ans = 0, exp = 1;
+	for(int i = length - 1; i >= 0; i--){
+		ans += a[i] * exp * (length - i);
+		exp *= x;
+	}
+	cout << ans <<endl;
+}
 int main()
 {
-    int x;
-    while(cin >> x){
-        string str;
-        getline(cin, str);
-        getline(cin, str);
-        int curr_index = 0;
-        string temp = "";
-        vector<int> poly;
-        for(int i = 0; i < str.length(); i++){
-            if(str[i] == ' '){
-                poly.push_back(stoi(temp));
-                temp = "";
-                curr_index = i + 1;
-            }
-            else{
-                temp += str[i];
-                if(i == str.length() - 1){
-                    poly.push_back(stoi(temp));
-                }
-            }
-        }
-        int ans = 0;
-        for(int i = 0; i < poly.size() - 1; i++){
-            ans += (poly.size() - i - 1) * poly[i] * pow(x, poly.size() - i - 2);
-        }
-        cout << ans << endl;
-    }
-    return 0;
+	int x, n;
+	while(scanf("%d", &x) != EOF){
+		for(n = 0; ; n++){
+            cin >> a[n];
+			if(getchar() == '\n'){
+				break;
+			}
+		}
+		run(n,x);
+	}
 }
